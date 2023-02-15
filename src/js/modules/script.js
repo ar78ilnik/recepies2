6a28283686c4d5953f4afb875257847d467fa2a9
ar78ilnik <ar78ilnik@gmail.com>
2023-02-15 16:38:49 +0300

    promo mobile

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,3 +1,12 @@
 export const func = () => {
-	console.log('1');
+    console.log('1');
+    const header = document.querySelector('.header');
+    const buttonMenu = header.querySelector('.button_menu');
+    const listMenu = header.querySelector('.list_menu');
+
+    buttonMenu.addEventListener('click', () => {
+        let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
+        buttonMenu.setAttribute('aria-expanded', !expanded);
+        buttonMenu.classList.toggle('button_menu--open');
+    })
 }
