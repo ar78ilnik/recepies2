6823d4617677bab269175ba3264370d7edaa2199
ar78ilnik <ar78ilnik@gmail.com>
2023-03-15 17:03:06 +0300

    header mobile

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -2,11 +2,12 @@ export const func = () => {
     console.log('1');
     const header = document.querySelector('.header');
     const buttonMenu = header.querySelector('.button_menu');
-    const listMenu = header.querySelector('.list_menu');
+    const listHeader = header.querySelector('.list_header');
 
     buttonMenu.addEventListener('click', () => {
         let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
         buttonMenu.setAttribute('aria-expanded', !expanded);
         buttonMenu.classList.toggle('button_menu--open');
+        listHeader.classList.toggle('list_header--open');
     })
 }
