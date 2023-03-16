88ebde71c8ca641d4e424306d389e9e842f1de4d
ar78ilnik <arakchiev.ilya@yandex.ru>
2023-03-16 22:11:38 +0300

    menu mobile

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -3,11 +3,13 @@ export const func = () => {
     const header = document.querySelector('.header');
     const buttonMenu = header.querySelector('.button_menu');
     const listHeader = header.querySelector('.list_header');
+    const navHeader = header.querySelector('.nav_header');
 
     buttonMenu.addEventListener('click', () => {
         let expanded = buttonMenu.getAttribute('aria-expanded') === 'true' || false;
         buttonMenu.setAttribute('aria-expanded', !expanded);
         buttonMenu.classList.toggle('button_menu--open');
+        navHeader.classList.toggle('nav_header--open');
         listHeader.classList.toggle('list_header--open');
     })
 }
