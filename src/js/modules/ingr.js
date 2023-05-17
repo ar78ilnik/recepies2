2aa30114e9d21e0596e20ab7bd66965ded6516f7
ar78ilnik <ar78ilnik@gmail.com>
2023-05-17 11:13:31 +0300

    grid column gap

---
+++ src/js/modules/ingr.js
@@ -0,0 +1,25 @@
+export const func = () => {
+    let options = document.querySelectorAll('.cal');
+    let optval = [];
+    let i = 0;
+    for (opt of options) {
+        optval[i] = opt.textContent;
+        i++;
+    }
+    i = 0;
+    for (opt of options) {
+        opt.textContent = optval[i] * 2;
+        i++;
+    }
+
+    function personSum() {
+        let sum = document.getElementById('doses').value;
+
+        i = 0;
+        for (opt of options) {
+            opt.textContent = optval[i] * sum;
+            i++;
+        }
+
+    }
+}
